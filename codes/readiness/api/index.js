c24b5250d76ab1445203994c695063bf2dddcf41
Angel Manuel Góez Giraldo <angelmanuel.goez@hotmail.es>
2023-06-14 20:40:09 -0500

    Exec example done

--- codes/readiness/api/index.js
+++ codes/readiness/api/index.js
@@ -1,16 +1,22 @@
-const mongoose = require('mongoose')
 const express  = require('express')
 const cors     = require('cors')
 const fs       = require('fs')
 const path     = require('path')
+const os       = require('os')
 
 const api = express()
 
 api.use(cors())
 api.use(express.json())
 
+console.log(`A file is going to get created in ${60 * 1000} miliseconds`)
+setTimeout(() => {
+  fs.writeFileSync(path.join('files', 'healthcheck'), "Exec test can run...")
+  console.log("The file was created")
+}, 60 * 1000)
+
 api.get('/healthcheck', (req, res) => {
-  res.send('OK')
+  res.send(`OK from ${os.hostname}`)
 })
 
 api.listen(8080, () => {
